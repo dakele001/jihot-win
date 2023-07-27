29a0a3d721b1376d97d7d5dd9f5529fc089bd1f2
dakele001 <89223287+dakele001@users.noreply.github.com>
2023-07-27 18:17:50 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '7dbd9431-455b-4af2-bc5c-58ea666f9dcb';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
